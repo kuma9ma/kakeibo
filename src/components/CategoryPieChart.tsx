4e95424a24d5f599f0793eeab7512c56eaf0a4f8
yamada <yamada@fistbump.biz>
2025-06-04 16:05:22 +0900

    デプロイ用修正

--- src/components/CategoryPieChart.tsx
+++ src/components/CategoryPieChart.tsx
@@ -52,7 +52,7 @@ const CategoryPieChart: React.FC<Props> = ({ items, type }) => {
             outerRadius={80}
             label
           >
-            {data.map((entry, idx) => (
+            {data.map((_entry, idx) => (
               <Cell key={`cell-${idx}`} fill={COLORS[idx % COLORS.length]} />
             ))}
           </Pie>
